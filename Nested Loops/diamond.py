d456ba7a865eb849d01f938f8bc7387abd66f077
Rishi Karthikeya Nori <rishikarthikeyanori@gmail.com>
2026-05-28 12:09:51 +0530

    pattern printing problems

---
+++ Nested Loops/diamond.py
@@ -0,0 +1,14 @@
+n=int(input("enter the size of the diamond: "))
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end=' ')
+    for j in range(1,2*i):
+        print("*",end=' ')
+    print()
+
+for i in range(n-1,0,-1):
+    for j in range(1,n-i+1):
+        print(" ",end=' ')
+    for j in range(1,2*i):
+        print("*",end=' ')
+    print()
